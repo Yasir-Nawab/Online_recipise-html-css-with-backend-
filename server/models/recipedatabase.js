3a01dfa37384833e2b5b0cf7da545602382e83c2
Yasir Nawab <115151765+Yasir-Nawab@users.noreply.github.com>
2024-03-02 18:12:37 +0500

    Update recipedatabase.js

--- server/models/recipedatabase.js
+++ server/models/recipedatabase.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/myrecipies");
+mongoose.connect("mongodb://localhost:0.0.0.0/myrecipies");
 
 // models
 require("./firstModel");
